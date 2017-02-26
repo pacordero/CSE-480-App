c29414ecd132f4aff0949aa6427886e0ece92573
pacordero <pacordero@oakland.edu>
2017-02-26 12:23:15 -0500

    android scr file

---
+++ src/main/java/com/example/patrickcordero/trailmix/Splash.java
@@ -0,0 +1,17 @@
+package com.example.patrickcordero.trailmix;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
